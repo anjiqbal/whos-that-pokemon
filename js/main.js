9320b74c49121291b245be6c04df133deee0755a
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-02-10 20:50:20 +0000

    added update check

--- js/main.js
+++ js/main.js
@@ -1,38 +1,24 @@
 //Example fetch using pokemonapi.co
-document.querySelector('#play').addEventListener('click', getFetch)
+
+document.querySelector('#play').addEventListener('click', play)
 document.querySelector('#check').addEventListener('click', checkAnswer)
+
+function play(){
+  getFetch()
+  document.querySelector('#check').addEventListener('click', checkAnswer)
+}
 
 function getFetch(){
   let randomNum = Math.floor(Math.random() * 1008)
   const url = 'https://pokeapi.co/api/v2/pokemon/'+randomNum
-
- 
-  let pokeStore = []
-  let pokeImg = []
-
-
+  let pokemonName 
+     
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        const pokemonName = data.name
-        //document.querySelector('h1').innerText = pokemonName
+        pokemonName = data.name
         document.querySelector('img').src = data.sprites.front_default
-
-        // pokeStore.push(data.types[0].type.name)
-        // pokeImg.push(data.sprites.front_shiny)
-        // pokeStore.push(data.types[0].type.name)
-        // pokeImg.push(data.sprites.front_shiny)
-    
-        // if((pokeStore[0] === "grass" && pokeStore[1] === 'water')){
-        //   document.querySelector('#pokeImg1').src = pokeImg[0]
-        //   document.querySelector('#pokeImg2').src = pokeImg[1]
-        //   document.querySelector('h2').innerText = " 2x > "
-        // }
-        
-        
-
-
       })
       .catch(err => {
           console.log(`error ${err}`)
@@ -40,7 +26,21 @@ function getFetch(){
 
 }
 
+function checkAnswer() {
+  console.log('hello')
+  let guess = document.querySelector('input').value
+  if(guess === pokemonName){
+    document.querySelector('#guessResult').innerText = 'You got it!'
+        document.querySelector('#answer').innerText = `It's ${pokemonName}!`
+    return true
+  } else {
+    document.querySelector('#guessResult').innerText = 'Wrong'
+    document.querySelector('#answer').innerText = `The correct answer is ${pokemonName}`
+    return false
+  }
+}
 
-
-
+function why() {
+  console.log('plz')
+}
 
